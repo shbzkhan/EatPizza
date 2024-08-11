9ad2f9c03f9e82831b57c681b2c0b28bb96a2072
Shahbaz Husain Khan <khanshahbazhusain@gmail.com>
2024-08-11 23:20:58 +0530

    add status selector for admin

--- src/app/(admin)/orders/[id].tsx
+++ src/app/(admin)/orders/[id].tsx
@@ -2,7 +2,9 @@ import orders from "@/assets/data/orders"
 import OrderListItem from "@/src/components/OrderLIstItem"
 import OrderItemListItem from "@/src/components/OrderItemListItem"
 import { Stack, useLocalSearchParams } from "expo-router"
-import { View, Text, FlatList } from "react-native"
+import { View, Text, FlatList, Pressable } from "react-native"
+import Colors from "@/src/constants/Colors"
+import { OrderStatusList } from "@/src/types"
 
 
 const OrderDetailScreen = ()=>{
@@ -25,6 +27,41 @@ const OrderDetailScreen = ()=>{
             renderItem={({item})=> <OrderItemListItem item = {item}/> }
             contentContainerStyle= {{gap: 10}}
             ListHeaderComponent={()=> <OrderListItem order = {order} /> }
+
+            ListFooterComponent={()=>
+            <>
+                <Text style={{ fontWeight: 'bold' }}>Status</Text>
+                <View style={{ flexDirection: 'row', gap: 5 }}>
+                {OrderStatusList.map((status) => (
+                    <Pressable
+                        key={status}
+                        onPress={() => console.warn('Update status')}
+                        style={{
+                            borderColor: Colors.light.tint,
+                            borderWidth: 1,
+                            padding: 10,
+                            borderRadius: 5,
+                            marginVertical: 10,
+                            backgroundColor:
+                        order.status === status
+                            ? Colors.light.tint
+                            : 'transparent',
+                        }}
+                    >
+                        <Text
+                        style={{
+                            color:
+                            order.status === status ? 'white' : Colors.light.tint,
+                        }}
+                        >
+                            {status}
+                        </Text>
+                    </Pressable>
+                    ))}
+                    </View>
+            </>
+            }
+
             />
         </View>
     )
